e2b52a6a0ef89e5691cca0ce4785aefeb2ae3fc0
jrdngill <jordangill600@gmail.com>
2020-10-09 18:04:28 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
+<!--
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+     Learn how to use images here: https://gridsome.org/docs/images 
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world!</h1>
@@ -14,8 +15,14 @@
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
-
+    </div>
   </Layout>
+  -->
+  <div class="soon">  
+    <img src="https://media4.giphy.com/media/j0ph697YBTGM8zm3u8/giphy.gif" alt="fox hiding in grass">
+    <p>Coming soon'ish, tis a work in progress my friends.</p>
+    <a href="https://twitter.com/jrdngill">@jrdngill</a>
+  </div>
 </template>
 
 <script>
@@ -29,5 +36,13 @@ export default {
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+.soon {
+  text-align: center;
+  padding-top: 100px;
+}
+.soon a {
+  color:#426342;
+  
 }
 </style>
